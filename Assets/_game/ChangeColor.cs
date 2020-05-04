8e5cff2dff3d5f81359d270d320da2f2e8d27f02
AdalZaleta <32682010+AdalZaleta@users.noreply.github.com>
2020-05-04 15:36:36 -0500

    COSAS

--- Assets/_game/ChangeColor.cs
+++ Assets/_game/ChangeColor.cs
@@ -1,21 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
+
+public enum Herramienta
+{
+    Regadera,
+    Guante,
+    Semillas,
+    Nabo,
+    Radio
+}
 
 public class ChangeColor : MonoBehaviour
 {
+    public float timeToWait;
+    public Herramienta miHerramienta;
+    public TextMeshProUGUI info;
+    public AudioClip[] music;
+    public int idexSong;
+
     public void Red()
     {
         GetComponent<Renderer>().material.color = Color.red;
+        StopCoroutine("PowerUP");
     }
 
     public void Blue()
     {
         GetComponent<Renderer>().material.color = Color.blue;
+        Debug.Log("Hola");
+        StartCoroutine("PowerUP", timeToWait);
     }
 
     public void Black()
     {
         GetComponent<Renderer>().material.color = Color.black;
+    }
+
+    public IEnumerator PowerUP(float _t)
+    {
+        yield return new WaitForSeconds(_t);
+        Debug.Log("Ya pasaron los 2 segundos");
+        GetComponent<Renderer>().material.color = Color.green;
+        info.text = "Herramienta: " + miHerramienta.ToString();
+        if(miHerramienta == Herramienta.Radio)
+        {
+            ChangeSong();
+        }
+    }
+
+    public void ChangeSong()
+    {
+        Debug.Log(music.Length.ToString());
+        idexSong = (idexSong < music.Length - 1  ? idexSong + 1 : 0);
+        GetComponent<AudioSource>().clip = music[idexSong];
+        Debug.Log("Rola actual: " + music[idexSong].name);
+        GetComponent<AudioSource>().Play(0);
     }
 }
